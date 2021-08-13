efd0bd29e0baee2231b659636a868313f7444159
Manali_Mrunali_Babar <88653010+MrunaliBabar@users.noreply.github.com>
2021-08-13 10:15:32 +0530

    Assignment 8

--- Jpr7.java
+++ Jpr7.java
@@ -10,13 +10,13 @@ public class Jpr7 {
 		System.out.println(result);// result is 3
 		result--;
 		System.out.println(result);// result value is 2
-		result=*2;
+		result*=2;
 		System.out.println(result);// result value is 4
-		result=/2;
+		result/=2;
 		System.out.println(result);// result value is 2
-		result=+8;
+		result+=8;
 		System.out.println(result);// result value is 10
-		result=%7;
+		result%=7;
 		System.out.println(result);// result value is 3
 
 
